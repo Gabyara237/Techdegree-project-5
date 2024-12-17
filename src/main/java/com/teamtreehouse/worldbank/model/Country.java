906cdc5c78ef40542b10bcbc7372b924d2f21031
Gabriela Araujo <110007206+Gabyara237@users.noreply.github.com>
2024-12-17 13:22:54 -0800

    Develop a Country class that maps to the database table structure. Use appropriate Hibernate annotations for mapping. Implement getters and setters.

--- src/main/java/com/teamtreehouse/worldbank/model/Country.java
+++ src/main/java/com/teamtreehouse/worldbank/model/Country.java
@@ -1,5 +1,55 @@
 package com.teamtreehouse.worldbank.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+
 public class Country {
 
+    @Id
+    private String code;
+    @Column
+    private String name;
+    @Column
+    private double internetUsers;
+    @Column
+    private double adultLiteracyRate;
+
+
+    // Default constructor for JPA
+    public Country(){}
+
+
+    // #######  Getters and Setters ######
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getInternetUsers() {
+        return internetUsers;
+    }
+
+    public void setInternetUsers(double internetUsers) {
+        this.internetUsers = internetUsers;
+    }
+
+    public double getAdultLiteracyRate() {
+        return adultLiteracyRate;
+    }
+
+    public void setAdultLiteracyRate(double adultLiteracyRate) {
+        this.adultLiteracyRate = adultLiteracyRate;
+    }
 }
